d77c61c035a39caab14f8cfb7ed0043a393ff214
Markus Liebelt <mliebelt@hey.com>
2025-12-20 18:11:25 +0100

    #5 Upgraded Docusaurus to get rid of some errors

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,6 +13,7 @@ const config: Config = {
   url: "https://lichess-studies-doc.netlify.app/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
+  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
